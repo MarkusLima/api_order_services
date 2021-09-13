1e4fb7e34008c84e31ff3bebc0d2b6dee032c48b
Markus Lima <markusmak@gmail.com>
2021-09-13 18:35:57 -0300

    create migrations

---
+++ database/migrations/2021_09_13_212039_create_orders_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->index('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->decimal('price', 22)->default(0.00);
+
+            $table->unsignedBigInteger('status_id');
+            $table->index('status_id');
+            $table->foreign('status_id')->references('id')->on('statuses')->onDelete('cascade');
+
+            $table->unsignedBigInteger('executor')->nullable();
+            $table->index('executor');
+            $table->foreign('executor')->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
